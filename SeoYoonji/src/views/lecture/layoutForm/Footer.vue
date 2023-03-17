6b5d59d6152e3a1cc198ccb9a262ecc300dce8a3
dduru-nuna <syj00062@gmail.com>
2023-03-17 19:50:50 +0900

    fix: [서윤지] footer 위치 변경

--- SeoYoonji/src/views/lecture/layoutForm/Footer.vue
+++ SeoYoonji/src/views/lecture/layoutForm/Footer.vue
@@ -1,38 +1,42 @@
 <template>
-  <v-footer app inset padless class="justify-center pl-0">
-    <v-card flat class="text-center" width="100%">
+    <v-footer inset padless class="justify-center pl-0">
+      <v-card flat class="text-center" width="100%">
         <v-card-text>
-            <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
-                <v-icon size="24px">
-                    {{ icon }}
-                </v-icon>
-            </v-btn>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
         </v-card-text>
         <v-card-text class="pt-0">
-            망고요구르트
+          망고 요구르트
         </v-card-text>
         <v-divider></v-divider>
-        <v-card-text>
-            {{ new Date().getFullYear() }} -
-            <strong>YOONJI</strong>
+        <v-card-text class="blue--text">
+          {{ new Date().getFullYear() }} -
+          <strong>YOONJI</strong>
         </v-card-text>
-    </v-card>
-  </v-footer>
-</template>
-
-<script>
-export default {
-    name: "Footer",
-    data: () => ({
-        icons: [
-            "mdi-facebook",
-            "mdi-twitter",
-            "mdi-google-plus",
-        ]
-    })
-}
-</script>
-
-<style>
-
-</style>
+      </v-card>
+    </v-footer>
+  </template>
+  
+  <script>
+  
+  export default {
+      name: "Footer",
+      data: () => ({
+          icons: [
+              "mdi-facebook",
+              "mdi-twitter",
+              "mdi-google-plus",
+              "mdi-linkedin",
+              "mdi-instagram"
+          ]
+      })
+  }
+  
+  </script>
+  
+  <style>
+  
+  </style>
